a744a56ffae4aba73ec50dc1709c9ce33f88d8b4
mohan <mohan@mohan-PC>
2019-11-16 18:44:36 +0530

    added data provider test case

---
+++ POM/src/pomtestcases/dataprovider.java
@@ -0,0 +1,29 @@
+package pomtestcases;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class dataprovider {
+	
+	@DataProvider
+	public Object[][] mydata()
+	{
+		Object[][] data=new Object[3][2];
+		data[0][0]="first";
+		data[0][1]="second";
+		data[1][0]="third";
+		data[1][1]="fourth";
+		data[2][0]="fifth";
+		data[2][1]="sixth";
+		return data;
+	}
+	
+@Test(dataProvider ="mydata")
+	public void startthecar(String username,String password)
+	{
+	System.out.println("startthecar");
+	System.out.println(username);
+	System.out.println(password);
+	}
+
+}
